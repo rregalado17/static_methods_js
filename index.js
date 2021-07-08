7e8565d53ec0f18a2a591a6653819392fab6e4d8
rregalado17 <rregalado17@gmail.com>
2021-07-08 14:25:58 -0500

    static methods review

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[$@!#^*({]/g,'')
+  }
+
+  static titleize(string){
+    let allowed = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let words = string.split(' ')
+    for (let i = 0; i < words.length; i++ ) {
+      if (i === 0 ){
+        result.push(this.capitalize(words[i]))
+      } else {
+        if (allowed.includes(words[i])){
+          result.push(words[i])
+        } else {
+          result.push(this.capitalize(words[i]))
+        }
+      }
+    }
+    return result.join(' ')
+  }
 }
